eb772acd0c3aab428dba185ecdfd3124bd1eec1d
xiliahz <dujunjie584@gmail.com>
2023-11-27 21:05:33 +0800

    thyme :build for crDroid-13

--- lineage_thyme.mk
+++ lineage_thyme.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2022 The KaleidoscopeOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,20 +8,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from thyme device
 $(call inherit-product, device/xiaomi/thyme/device.mk)
 
+# Inherit common LineageOS configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit GApps configurations
+$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_thyme
 PRODUCT_DEVICE := thyme
-PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2102J2SC
+PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/thyme/thyme:13/RKQ1.211001.001/V14.0.5.0.TGACNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/thyme/thyme:12/SKQ1.211006.001/V13.0.8.0.SGACNXM:user/release-keys
